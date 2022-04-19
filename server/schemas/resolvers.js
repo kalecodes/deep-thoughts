b2c5c9a51a05d31ec270937a4dda8e66199c3524
kalen510 <kalen.wiley@gmail.com>
2022-04-18 21:35:14 -0400

    thought and reaction types

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,18 @@
+const { User, Thought } = require("../models");
+
 // resolver that will serve the response for the query
 const resolvers = {
     Query: {
+        // pass in parent as a placeholder
         // methods get the same name of the query or mutation they are for
-        helloWorld: () => {
-            return 'Hello world!';
+        thoughts: async (parent, { username }) => {
+            // use a ternary operator to check if usernmae exists
+            // if username exists, we set params to an object with a 'username' key as its value
+            // if it doesn't, we return an empty object
+            const params = username ? { username } : {};
+            // pass params object (with or without date)  to our .find method
+            // return all thought data in descending order
+            return Thought.find(params).sort({ createdAt: -1 });
         }
     }
 };
